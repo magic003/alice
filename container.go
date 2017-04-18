c6cc8708d0c3e72b4c846f694bf8fe3d46e99e4f
Minjie Zha <minjiezha@gmail.com>
2017-04-18 09:04:18 -0700

    Update documentation for Container methods

--- container.go
+++ container.go
@@ -19,10 +19,10 @@ func CreateContainer(modules ...Module) Container {
 // Container defines the interface of an instance container. It initializes instances based on dependencies,
 // and provides APIs to retrieve instances by type or name.
 type Container interface {
-	// Instance returns an instance by type. It panics if there are multiple instances for the same type.
-	// It returns nil if no instance is found.
+	// Instance returns an instance by type. It panics when no instance is found,
+	// or multiple instances are found for the same type.
 	Instance(t reflect.Type) interface{}
-	// InstanceByName returns an instance by name. It returns nil if no instance is found.
+	// InstanceByName returns an instance by name. It panics when no instance is found.
 	InstanceByName(name string) interface{}
 }
 
